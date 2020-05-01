19eeadc132bc94548b7ad60e04128b71be24b604
samuel martinez <samuel.martinez.morales@gmail.com>
2020-05-01 11:58:46 -0500

    Cambiar titulo de forma condicional

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
   const eliminarCita = id => {
     const nuevasCitas = citas.filter(cita => cita.id !== id);
     guardarCitas(nuevasCitas)
-
   }
+  const titulo = citas.length === 0 ? 'No Hay  citas' : 'Administra tus citas'
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -28,7 +29,7 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            <h2>Gestiona las citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita =>(
               <Cita 
                 key={cita.id}
